1298c01797bf4dbd1dbdde37da16263f9ae6f63f
AnatolijDm <anat_dr25@rambler.ru>
2020-03-14 13:49:39 +0300

    feat: add half of solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    let dfd = [];
+    let ddd = str.split('');
+    dfd.push(ddd);
+    if (dfd.toString() === bracketsConfig.toString()) {
+        return true;
+    } else return false;
 }
